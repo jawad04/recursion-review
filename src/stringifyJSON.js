fbe85309cab7e669477b5e4f58907c221aee2a15
jawad04 <muhammadijawad10@gmail.com>
2018-09-17 15:33:08 -0700

    done with stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -21,6 +21,29 @@ var stringifyJSON = function(obj) {
   if(Array.isArray(obj) && obj.length === 0){
     return '[]';
   }
+
+  var objectRecursion = function (object){
+    var objArray = [];
+    for(var key in object){
+      if(typeof object[key] === 'string'){
+        objArray.push('"' + key + '"' + ':' + '"' + object[key] + '"');
+      }
+      if(object[key] === null){
+        objArray.push('"' + key + '"' + ':' + 'null');
+      }
+      if(typeof object[key] === 'number' || typeof object[key] === 'boolean'){
+      objArray.push('"' + key + '"' + ':' + object[key]); 
+      }
+      if(Array.isArray(object[key])){
+        objArray.push('"' + key + '"'+ ':' + '[' + arrayRecursion(object[key]));
+      }
+      if(typeof object[key] === 'object' && object[key] !== null && !Array.isArray(object[key])){
+        objArray.push('"' + key + '"' + ':' + objectRecursion(object[key]));
+      }
+    }
+    return '{' + objArray + '}';
+  }
+
   var arrayRecursion = function (arr) {
     if(arr.length === 0){
       return ']';
@@ -40,6 +63,12 @@ var stringifyJSON = function(obj) {
     if(Array.isArray(arr[0])){
       return '[' + arrayRecursion(arr[0]) + arrayRecursion(arr.slice(1));
     }
+    if(typeof arr[0] === 'object'){
+      if(arr.length > 1){
+        return objectRecursion(arr[0]) + ',' + arrayRecursion(arr.slice(1));
+      }
+      return objectRecursion(arr[0]) + arrayRecursion(arr.slice(1));
+    }
     if(arr.length > 1){
       return arr[0] + ',' + arrayRecursion(arr.slice(1));
     }
@@ -51,17 +80,8 @@ var stringifyJSON = function(obj) {
  
   //code for objects
 
-  var objectRecursion = function (object){
-    var objArray = [];
-    for(var key in object){
-      objArray.push('"', key, '"');
-      objArray.push(':');
-      objArray.push('"', object[key], '"');
-      
-    }
-    return '{' + objArray.join('') + '}';
-  }
-
+  
+ 
   if(typeof obj === 'object'){
     return objectRecursion(obj);
   }
