90937912072c946114d89f33b254e6674a2da461
jawad04 <muhammadijawad10@gmail.com>
2018-09-17 16:18:09 -0700

    done with get elements by class name

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,28 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  //declare array
+  //write recursive function
+  //check each node to see if the className matches
+  //if it does, then push into array
+  //call function to check every node
+
+var elementsArr = [];
+
+  var checkNode = function(node){
+    if(node.className.includes(className)){
+      elementsArr.push(node);
+    }
+    if(node.nextElementSibling !== null){
+      checkNode(node.nextElementSibling);
+    }
+    if(node.firstElementChild !== null){
+      checkNode(node.firstElementChild);
+    }
+    //console.log(elementsArr);
+    //return [];
+  }
+  
+  checkNode(document.body);
+  return elementsArr;
 };
